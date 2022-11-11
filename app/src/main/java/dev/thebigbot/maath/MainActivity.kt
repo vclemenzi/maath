8091b358f156186a4bdfe9f40becf1c401a2b588
aThebigbot <thebigboss.garibaldi@gmail.com>
2022-11-11 19:17:25 +0100

    feat(ui): first draft of style

---
+++ app/src/main/java/dev/thebigbot/maath/MainActivity.kt
@@ -0,0 +1,16 @@
+package dev.thebigbot.maath
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Remove action bar
+        supportActionBar?.hide()
+    }
+}
